02f48c6dea4b264a339c21a3912d810a92d05284
Adil <a@a.com>
2023-11-11 14:45:24 +0100

    Install pytest

--- books/tests.py
+++ books/tests.py
@@ -1,26 +1,48 @@
+import pytest
+import random
 from typing import List, Optional
-from django.contrib.auth.models import User, Group
+from .models import Book
+from authors.models import Author
+from proj.lib import random_field, random_date
+from django_countries.fields import countries
 
-def create_app_user(
-    username: str,
-    password: Optional[str] = None,
-    first_name: Optional[str] = "first name",
-    last_name: Optional[str] = "last name",
-    email: Optional[str] = "foo@bar.com",
-    is_staff: str = False,
-    is_superuser: str = False,
-    is_active: str = True,
-    groups: List[Group] = [],
-) -> User:
-    user = User.objects.create_user(
-        username=username,
-        password=password,
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        is_staff=is_staff,
-        is_superuser=is_superuser,
-        is_active=is_active,
-    )
-    user.groups.add(*groups)
-    return user
+@pytest.fixture
+def author_factory(db):
+    def create_author(
+        last_name=random_field(20),
+        middle_name=random_field(20),
+        first_name=random_field(20),
+        country=random.choice(list(countries)),
+        birth_date=random_date()
+    ):
+        return Author(
+            last_name=last_name,
+            middle_name=middle_name,
+            first_name=first_name,
+            country=country,
+            birth_date=birth_date
+        )
+    return create_author
+
+@pytest.fixture
+def author_A(db, author_factory):
+    return author_factory()
+
+'''
+@pytest.fixture
+def book_factory(db):
+    def create_book(
+        author =
+    ) -> Book:
+
+        user.groups.add(*groups)
+        return user
+'''
+
+def test(db, author_A):
+    print(author_A)
+    assert "A" == "A"
+    
+def test_2(db, author_A):
+    print(author_A)
+    assert "A" == "A"
--- proj/lib.py
+++ proj/lib.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import string, random
+
 from django import forms
 
 class CustomDateField(forms.DateField):
@@ -9,3 +11,13 @@ class CustomDateField(forms.DateField):
             except ValueError:
                 pass
         return super().to_python(value)
+    
+def random_field(N):
+    chars = string.ascii_letters + string.digits + string.whitespace
+    return "".join(random.choices(chars, k=N))
+
+def random_date():
+    return datetime.strptime(
+        f"{random.randint(1,12)}/{random.randint(1,28)}/{random.randint(1900, 2023)}",
+        "%m/%d/%Y"
+    )
